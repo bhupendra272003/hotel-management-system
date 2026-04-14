250acdcd7ed369822d169941bb09860adaed4f30
BHUPENDRA SINGH DEORA <122671807+bhupendra272003@users.noreply.github.com>
2026-04-14 10:32:35 +0530

    Update TaskDistribution.jsx

--- frontend/src/components/admin/TaskDistribution.jsx
+++ frontend/src/components/admin/TaskDistribution.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import API_URL from "../../api/config";
 
 export default function TaskDistribution() {
   const [staff, setStaff] = useState([]);
